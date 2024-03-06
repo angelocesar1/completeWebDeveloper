fef06a8bea93f35bab50445c3470fa0235971aa8
Angelo Dos Santos <adossantos@Zephyrus>
2024-03-06 11:22:09 -0600

    created a calculator using JS

---
+++ javaScript/exercise3/exercise3.js
@@ -0,0 +1,29 @@
+
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+// 1. Prompts the user for first number.
+// 2. Stores that first number
+// 3. Prompts the user for the second number.
+// 4. stores that number and responds with the SUM by using an alert.  
+
+var firstNumber = prompt("Please enter the first number: ")
+var secondNumber = prompt("Please enter the second number: ")
+
+var sum = alert(Number(firstNumber) + Number(secondNumber))
+
+
+
+// BONUS: Make a program that can subtract, multiply, and also divide!
+
+var firstNumber = prompt("Please enter the first number: ")
+var secondNumber = prompt("Please enter the second number: ")
+var action = prompt("Please list whether you want to: Multiply, Divide, Add, or Subtract")
+
+if (action == 'divide' || action == 'Divide') {
+    var divide = alert(Number(firstNumber) / Number(secondNumber))
+} else if (action == 'multiply' || action == 'Multiply') {
+    var multiply = alert(Number(firstNumber) * Number(secondNumber))
+} else if (action == 'add' || action == 'Add') {
+    var sum = alert(Number(firstNumber) + Number(secondNumber))
+} else if (action == 'subtbract' || action == 'Subtract') {
+    var difference = alert(Number(firstNumber) - Number(secondNumber))
+}
